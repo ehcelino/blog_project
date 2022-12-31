5248040222e1a4aa93d73151e52f1dec6a454eee
Eduardo Celino <ehcelino@gmail.com>
2022-12-30 22:06:16 -0300

    correção no timeout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
-  # before_action :check_timeout
+  before_action :check_timeout
   before_action :current_user
 
 
   def check_timeout
-    timeout_period = 1.minutes
+    timeout_period = 10.minutes
     if current_user && session[:last_request_at] && session[:last_request_at] < timeout_period.ago
       # current_user = nil
       # reset_session
-      session[:user_id] = nil
+      # session[:user_id] = nil
+      cookies.delete(:auth_token)
       redirect_to new_session_path, alert: "Sua sessão foi finalizada. Por favor faça login novamente."
     else
       session[:last_request_at] = Time.current
